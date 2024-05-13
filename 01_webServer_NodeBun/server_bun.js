7974119c6b6573e8b8a816f9a86445e12e14d693
Ahindra D <official.ahindra@gmail.com>
2024-05-13 12:23:13 +0530

    bun_resp

--- 01_webServer_NodeBun/server_bun.js
+++ 01_webServer_NodeBun/server_bun.js
@@ -8,13 +8,13 @@ serve({
         console.log(req.url)
         const url= new URL(req.url)
         if(url.pathname==="/"){
-            return new Response({message:"Hello from Bun server"},{status:200})
+            return new Response("Hello from Bun server",{status:200})
         }
         else if(url.pathname==="/me"){
-            return new Response({message:"Hello ME from Bun server"},{status:200})
+            return new Response("Hello ME from Bun server",{status:200})
         }
         else{
-            return new Response({message:"Not found 404"},{status:404})
+            return new Response("Not found 404",{status:404})
         }
     },
     port: PORT,
