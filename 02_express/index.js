2b53a94647c58370f6042953a780e06f6d2923a0
Ahindra D <official.ahindra@gmail.com>
2024-05-20 12:40:04 +0530

    postman_randomDataVar

--- 02_express/index.js
+++ 02_express/index.js
@@ -17,12 +17,31 @@ const USER_DATA=[];
 let nextUserID=1;
 
 app.post("/users",(req,res)=>{
+    ```
+    postman post calls with random data
+    {
+    "name":"{{$randomFullName}}",
+    "email":"{{$randomEmail}}"
+    }
+    ```
     const{name,email}=req.body;
     if(!name || !email){res.status(400).json({message:"name and email are required"});}
     const userData={id:nextUserID++,name,email};
     USER_DATA.push(userData);
     res.status(201).json(userData);
 })
+
+app.get("/users/:id",(req,res)=>{
+    const ID=req.params.id;
+    console.log('ID',ID);
+    const curr_user=USER_DATA.find(user=>user.id===parseInt(ID));
+    console.log('curr_user',curr_user);
+    if(!curr_user){res.status(404).json({message:"user not found"});}
+    res.json(curr_user).status(200);
+})
+
+
+
 
 app.listen(PORT,()=>{
     console.log(`server is running on port: ${PORT} .....`);
