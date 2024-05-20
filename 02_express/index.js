c4cb6a8cdc2c4aa88475d1a13c2fb7f4321bbe6e
Ahindra D <official.ahindra@gmail.com>
2024-05-20 13:39:50 +0530

    CRUD

--- 02_express/index.js
+++ 02_express/index.js
@@ -2,23 +2,22 @@ import express from "express";
 
 const app=express();
 const PORT=process.env.PORT || 3000;
-
 app.use(express.json());//only json data will be accepted
 
 app.get("/",(req,res)=>{
     res.json({message:"hello from Express"},200);
 })
-app.get("/users",(req,res)=>{
-    res.json({users:USER_DATA},200);
-})
 
 
 const USER_DATA=[];
 let nextUserID=1;
+//C_R_U_D operations
 
+//Add user
 app.post("/users",(req,res)=>{
     ```
-    postman post calls with random data
+    https://learning.postman.com/docs/writing-scripts/script-references/variables-list/
+    ---postman post calls with random data
     {
     "name":"{{$randomFullName}}",
     "email":"{{$randomEmail}}"
@@ -31,6 +30,12 @@ app.post("/users",(req,res)=>{
     res.status(201).json(userData);
 })
 
+// Get all users
+app.get("/users",(req,res)=>{
+    res.json({users:USER_DATA},200);
+})
+
+//Get a user with ID
 app.get("/users/:id",(req,res)=>{
     const ID=req.params.id;
     console.log('ID',ID);
@@ -40,6 +45,29 @@ app.get("/users/:id",(req,res)=>{
     res.json(curr_user).status(200);
 })
 
+
+//Update a user
+app.patch("/users/:id",(req,res)=>{
+    const ID=req.params.id;
+    const {name,email}=req.body;
+    const curr_user=USER_DATA.find(user=>user.id===parseInt(ID));
+    if(!curr_user){res.status(404).json({message:"user not found"});}
+    if(!name || !email){res.status(400).json({message:"name and email are required"});}
+    curr_user.name=name;
+    curr_user.email=email;
+    res.json(curr_user).status(200);
+})
+
+
+//Delete a user
+app.delete("/users/:id",(req,res)=>{
+    const ID=req.params.id;
+    const user_index=USER_DATA.findIndex(user=>user.id===parseInt(ID));
+    if(index===-1){res.status(404).json({message:"user not found"});}
+    USER_DATA.splice(user_index,1);
+    return res.status(204).json({message:"user deleted"});
+    //what is 204 status code? it means no content
+})
 
 
 
